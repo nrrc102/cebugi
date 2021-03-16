69930bfc572aee0f83dcd03c5bbf38639c0d9ae4
Nell Ryan Codilla <47083395+nrrc102@users.noreply.github.com>
2021-03-16 19:06:21 +0800

    Update Directory.js

--- src/components/directory/Directory.js
+++ src/components/directory/Directory.js
@@ -1,25 +1,10 @@
 import React from 'react';
-<<<<<<< HEAD
-// import ShopMen from './../../assets/shopMens.jpg';
-// import ShopWomen from './../../assets/shopWomens.jpg';
-=======
->>>>>>> 58ab4a729549fa2a8b87159d8201151e9e645221
 import './styles.scss';
 
 export default function Directory(props) {
     return (
         <div className="directory">
-<<<<<<< HEAD
-            {/* <div className="wrap">
-            <div className="item" style={{backgroundImage: `url(${ShopWomen})`}}>
-                <a> Shop Womens</a>
-            </div>
-            <div className="item" style={{backgroundImage: `url(${ShopMen})`}}>
-                <a>Shop Mens</a>
-            </div>
-            </div> */}
-=======
->>>>>>> 58ab4a729549fa2a8b87159d8201151e9e645221
+
         </div>
     )
 }
